d2e6703603ee736b0c9597dae1a0b2bae13658cb
Jules Roman Polidario <jrpolidario@yahoo.com>
2016-05-23 09:16:43 +0000

    command: rails generate controller about index contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'about/index'
+
+  get 'about/contact'
+
   resources :posts
   root 'posts#index'
   # The priority is based upon order of creation: first created -> highest priority.
